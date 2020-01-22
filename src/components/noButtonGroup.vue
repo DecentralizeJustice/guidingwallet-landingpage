1fe10d4feb840a13a23c27e1855de269a6728fe4
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-01-22 01:20:21 -0500

    Added coin info

---
+++ src/components/noButtonGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row
+  justify="space-around"
+  >
+    <v-col
+      v-for="(card, index) in cards" :key="`card-${index}`"
+      cols="3"
+    >
+    <v-card flat class="transparent">
+      <v-card-text class="text-center">
+        <img class="" style="height:25vh;width:auto;"
+         :src='card.img'/>
+      </v-card-text>
+      <v-card-title primary-title class="layout justify-center">
+        <div class="headline text-center">{{card.title}}</div>
+      </v-card-title>
+      <v-card-text class="subtitle-1">
+        {{card.text}}
+      </v-card-text>
+    </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['cards'],
+  data: () => ({
+  })
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-      <section>
+    <v-container fluid class="pa-0">
         <v-parallax src="https://res.cloudinary.com/dylevfpbl/image/upload/v1579594531/landingpage/fit.jpg"
          height="600">
           <v-layout
@@ -17,70 +16,80 @@
           </v-card>
           </v-layout>
         </v-parallax>
-      </section>
-
-      <section>
-        <v-layout
-          column
-          wrap
-          class="my-12"
-          align-center
-        >
-          <v-flex xs12 sm4 class="">
+        <v-row
+        class="pt-8 pb-8"
+          >
+            <v-col
+            cols="12"
+            >
             <div class="text-center">
               <h2 class="display-1">The Guiding Wallet for The Decentralized World</h2>
             </div>
-          </v-flex>
-              <v-row
-              justify="space-around"
+            </v-col>
+          </v-row>
+          <noButtonGroup
+          v-bind:cards='valueProps'/>
+          <v-row
+          class="pt-5"
+            >
+              <v-col
+              cols="12"
               >
-                <v-col
-                  v-for="(card, index) in minicards" :key="`card-${index}`"
-                  cols="3"
-                >
-                <v-card flat class="transparent">
-                  <v-card-text class="text-center">
-                    <img class="" style="height:25vh;width:auto;"
-                     :src='card.img'/>
-                  </v-card-text>
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-center">{{card.title}}</div>
-                  </v-card-title>
-                  <v-card-text class="subtitle-1">
-                    {{card.text}}
-                  </v-card-text>
-                </v-card>
-                </v-col>
-              </v-row>
-        </v-layout>
-      </section>
-    </div>
+              <div class="text-center">
+                <h2 class="display-1">We Only Support Usefull Cryptos</h2>
+              </div>
+              </v-col>
+            </v-row>
+          <noButtonGroup
+          v-bind:cards='tokens'/>
+
+    </v-container>
 </template>
 
 <script>
+import noButtonGroup from '@/components/noButtonGroup.vue'
 export default {
   name: 'Main',
-
+  components: {
+    noButtonGroup
+  },
   data: () => ({
-    minicards: [
+    valueProps: [
       {
-        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579630770/landingpage/undraw_winners_ao2o.png',
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579632794/landingpage/undraw_winners_ao2o.svg',
         title: 'Only The Best of The Best',
         text: `Instead of hundreds of coins, Guide Wallet only supports the best coins that have a
-        proven track record, vast community support and specialized use cases.`
+        proven track record, vast community support, and specialized use cases.`
       },
       {
-        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579595188/landingpage/undraw_reading_list_4boi.png',
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579632829/landingpage/undraw_reading_list_4boi.svg',
         title: 'A High Return Investment',
         text: `Guide Wallet has a revolutionary curriculmn that allows you to learn
         about crypto. This curriculum helps you make a long lasting investment in yourself that will
         reward you for the rest of your life.`
       },
       {
-        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579594794/landingpage/undraw_conversation_h12g.png',
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579632764/landingpage/undraw_conversation_h12g.svg',
         title: 'Expert Guidance',
         text: `Guide Wallet offers you one-on-one help from a expert in
         cryptocurrency that allow you to fully explore and utlize the crypto-ecosystem.`
+      }
+    ],
+    tokens: [
+      {
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579664861/landingpage/bitcoin-btc-logo.svg',
+        title: 'Bitcoin for Savings',
+        text: ''
+      },
+      {
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579664858/landingpage/monero-xmr-logo.svg',
+        title: 'Monero for Privacy',
+        text: ''
+      },
+      {
+        img: 'https://res.cloudinary.com/dylevfpbl/image/upload/v1579664863/landingpage/dai-dai-logo.svg',
+        title: 'Dai for Stability and Loans',
+        text: ''
       }
     ]
   })
