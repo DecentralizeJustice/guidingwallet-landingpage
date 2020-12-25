8993f4366057e4c6fd4a9253e0b860cea204d13e
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2020-12-24 22:59:46 -0500

    update show time

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -429,7 +429,7 @@ export default {
     this.countDownTimer()
   },
   data: () => ({
-    startTime: 1608861600,
+    startTime: 1609117200,
     difference: 1000,
     prizeAmount: 20,
     heroSubtext: 'Guided Crypto Exploration',
