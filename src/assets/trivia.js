ecbea8826750d36988f0839ab5f57a902a312f8b
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2021-01-04 20:55:12 -0500

    copy change

--- src/assets/trivia.js
+++ src/assets/trivia.js
@@ -2,7 +2,7 @@ const info = {
   subject: 'Bitcoin Security',
   startTime: 1610672400,
   prizeAmount: 100,
-  paymentMethod: 'Monero',
+  paymentMethod: 'Bitcoin',
   numberOfQuestions: '10',
   timeToAnswer: '10 Seconds'
 }
