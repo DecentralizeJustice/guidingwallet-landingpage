a1453312c536b88b51cd9b3c19bb34fd5b4021ef
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2021-01-22 17:00:05 -0500

    Worked on more past trivia Shows

--- src/views/pastTrvia.vue
+++ src/views/pastTrvia.vue
@@ -55,7 +55,7 @@
 
            <v-col cols="8" md="3" style="background-color: #424242;" no-gutters
            class="text-center rounded my-3 pa-3 mx-2"
-           v-for="(fact, x) in showFacts"
+           v-for="(fact, x) in getFacts(this.latestShow)"
            :key="x"
            >
            {{fact[0]}}<br>
@@ -141,20 +141,128 @@
           </v-row>
           </v-col>
         </v-row>
-        <v-row class="ma-0 pa-0" align="center"
+        <v-row class="ma-0 pa-0" align="center" id='triviaHistory'
         justify="center" style="background-color: #0B5563;" no-gutters>
-          <v-col cols="12" md='9' style="" no-gutters class="mt-3"
+          <v-col cols="11" md='9' style="" no-gutters class="mt-3 text-center"
           >
+          <div class="text-h2 text-center white--text text-wrap my-5 font-weight-medium " :class="{'text-h2': $vuetify.breakpoint.mdAndUp, 'text-h4': $vuetify.breakpoint.smAndDown}">
+            More Past Trivia Shows</div>
+          <div class="text-h2 text-center white--text text-wrap my-5 font-weight-medium ">🧐</div>
         <v-expansion-panels>
-          <v-expansion-panel
+          <v-expansion-panel style="background-color: #212121;"
             v-for="(item,i) in oldShows"
             :key="i"
+            class="white--text"
           >
             <v-expansion-panel-header>
               1st Show - {{getDate(item.time)}}
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              {{item.info}}
+              <v-row class="ma-0 pa-0" align="center"
+              justify="center" style="background-color: #546E7A;" no-gutters>
+                <v-col cols="12" md='9' style="" no-gutters
+                >
+                <div
+                :class="{'text-h4': $vuetify.breakpoint.mdAndUp, 'text-h3': $vuetify.breakpoint.smAndDown}"
+                class="font-weight-medium text-left white--text ma-7 text-wrap"
+                style="overflow-wrap: break-word;">
+                 Summary: <br>
+                 <p class="text-subtitle-1 pa-3 mt-3 rounded" style="background-color: #616161;">{{item.info.summary}}</p>
+                </div>
+                <div
+                :class="{'text-h4': $vuetify.breakpoint.mdAndUp, 'text-h3': $vuetify.breakpoint.smAndDown}"
+                class="font-weight-medium text-left white--text ma-7 text-wrap"
+                style="overflow-wrap: break-word;">
+                 Trivia Info: <br>
+                 <v-row class="ma-0 pa-0" align="center" no-gutters justify='center' >
+                 <v-col cols="11" md="3" style="background-color: #424242;" no-gutters
+                 class="text-center rounded my-3 pa-3 mx-2"
+                 v-for="(fact, x) in getFacts(item.info)"
+                 :key="x"
+                 >
+                 {{fact[0]}}<br>
+                 <div class="ma-0 pa-0 text-h6">{{fact[1]}}</div>
+                 <v-icon
+                       x-large
+                       color="teal lighten-2"
+                     >
+                       mdi-{{fact[2]}}
+                     </v-icon>
+                </v-col>
+                </v-row>
+                </div>
+                <div
+                :class="{'text-h4': $vuetify.breakpoint.mdAndUp, 'text-h3': $vuetify.breakpoint.smAndDown}"
+                class="font-weight-medium text-left white--text ma-7 text-wrap"
+                style="overflow-wrap: break-word;">
+                 Link to Slide and Notes PDF: <br> <a :href="item.info.notesLink" target="_blank" >
+                   <v-btn style="background-color: #0B5563;" class="white--text mt-3">Click To View Notes</v-btn></a>
+                </div>
+                </v-col>
+                <v-col cols="12" md='9' style="" no-gutters
+                >
+                <div
+                :class="{'text-h4': $vuetify.breakpoint.mdAndUp, 'text-h3 text-center': $vuetify.breakpoint.smAndDown}"
+                class="font-weight-medium text-left white--text ma-7 text-wrap"
+                style="overflow-wrap: break-word;">
+                 Questions:
+                </div>
+                <v-row class="ma-0 pa-0" align="center"
+                justify="space-around" no-gutters>
+                <v-col cols="11" md="5" style="" no-gutters
+                v-for="index in 10" :key="index"
+                >
+                <v-card
+                  class="my-2 white--text text-left"
+                  style="background-color: #424242;"
+                >
+                  <v-card-text>
+                    <div class="white--text text-h4 text-center py-2">#{{index}}</div>
+                    <p class="display-1 white--text">
+                      {{item.info.questions[index].questionInfo.question}}
+                    </p>
+                    <v-list disabled style="background-color: #424242;">
+                      <v-list-item-group
+                      >
+                        <v-list-item
+                          v-for="(item, i) in item.info.questions[index].questionInfo.options"
+                          :key="i"
+                        >
+                          <v-list-item-content >
+                            <v-list-item-title v-text="item"
+                            class="text-h6 white--text"></v-list-item-title>
+                          </v-list-item-content>
+                        </v-list-item>
+                      </v-list-item-group>
+                    </v-list>
+                  </v-card-text>
+                  <v-card-actions class="justify-center">
+                    <v-btn
+                      color="blue lighten-5"
+                      @click="showAnswer(item.time + '-' + index)"
+                    >
+                      Show Answer
+                    </v-btn>
+                  </v-card-actions>
+                  <v-expand-transition>
+                    <v-card
+                    flat rounded='0'
+                      v-if="showAnswerInfo[item.time + '-' + index] !== undefined"
+                      class="transition-fast-in-fast-out v-card--reveal"
+                      style="background-color: #0B5563;"
+                    >
+                      <v-card-text class="">
+                        <p class="display-1 white--text">
+                          {{item.info.questions[index].questionInfo.options[item.info.questions[index].answer]}}
+                        </p>
+                      </v-card-text>
+                    </v-card>
+                  </v-expand-transition>
+                </v-card>
+                </v-col>
+                </v-row>
+                </v-col>
+              </v-row>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -177,6 +285,7 @@ export default {
   },
   methods: {
     showAnswer: function (index) {
+      console.log(index)
       const clone = Object.assign({}, this.showAnswerInfo)
       clone[index] = true
       this.showAnswerInfo = clone
@@ -187,6 +296,16 @@ export default {
       const month = d.getMonth() + 1
       const year = d.getFullYear()
       return day + '/' + month + '/' + year
+    },
+    getFacts: function (showInfo) {
+      const info = []
+      info.push([showInfo.potSize + ' USD', 'Potsize', 'cash'])
+      info.push([showInfo.subject, 'Subject', 'book'])
+      info.push(['#' + showInfo.killerQuestion, 'Most Missed Question', 'bullseye-arrow'])
+      info.push([showInfo.totalWinners, 'Total Winners', 'emoticon-cool-outline'])
+      // info.push([this.latestShow.potPerWinner + ' USD', 'Each Winners Prize', 'account-cash'])
+      info.push([showInfo.paymentMethod, 'Payment Crypto', showInfo.paymentMethodIcon])
+      return info
     }
   },
   computed: {
@@ -196,16 +315,6 @@ export default {
       const month = d.getMonth() + 1
       const year = d.getFullYear()
       return date + '/' + month + '/' + year
-    },
-    showFacts: function () {
-      const info = []
-      info.push([this.latestShow.potSize + ' USD', 'Potsize', 'cash'])
-      info.push([this.latestShow.subject, 'Subject', 'book'])
-      info.push(['#' + this.latestShow.killerQuestion, 'Most Missed Question', 'bullseye-arrow'])
-      info.push([this.latestShow.totalWinners, 'Total Winners', 'emoticon-cool-outline'])
-      // info.push([this.latestShow.potPerWinner + ' USD', 'Each Winners Prize', 'account-cash'])
-      info.push([this.latestShow.paymentMethod, 'Payment Crypto', this.latestShow.paymentMethodIcon])
-      return info
     },
     sortedKeys: function () {
       const keys = Object.keys(gameInfo.default)
